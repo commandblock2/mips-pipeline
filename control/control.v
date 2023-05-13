e099d5ca1a32b8ce9f70736f77087df00f4b2334
commandblock2 <tony_hu@live.com>
2023-05-13 17:41:04 +0800

    renames and control aka decoder implemented

---
+++ control/control.v
@@ -0,0 +1,93 @@
+/*
+ * This module takes the 6 bit opcode, and gives if some wires should be connected
+*/
+
+`ifndef _control
+`define _control
+
+        module control(
+                input wire [5:0] opcode,
+                output reg branch_eq, branch_ne,
+                output reg [1:0] alu_opcode,
+                output reg memory_read, memory_write, memory_to_register,
+                output reg register_destination, register_write, alu_source,
+                output reg jump
+            );
+
+            // Constants
+            localparam LW = 6'b100011;
+            localparam BEQ = 6'b000100;
+            localparam BNE = 6'b000101;
+            localparam SW = 6'b101011;
+            localparam ADDI = 6'b001000;
+            localparam ADD = 6'b000000;
+            localparam JUMP = 6'b000010;
+
+            always @(*)
+            begin
+                // Defaults
+                alu_opcode[1:0] <= 2'b10;
+                alu_source <= 1'b0;
+                branch_eq <= 1'b0;
+                branch_ne <= 1'b0;
+                memory_read <= 1'b0;
+                memory_to_register <= 1'b0;
+                memory_write <= 1'b0;
+                register_destination <= 1'b1;
+                register_write <= 1'b1;
+                jump <= 1'b0;
+
+                case (opcode)
+                    LW: 
+                    begin
+                        memory_read <= 1'b1;
+                        register_destination <= 1'b0;
+                        memory_to_register <= 1'b1;
+                        alu_opcode[1] <= 1'b0;
+                        alu_source <= 1'b1;
+                    end
+
+                    BEQ: 
+                    begin
+                        alu_opcode[0] <= 1'b1;
+                        alu_opcode[1] <= 1'b0;
+                        branch_eq <= 1'b1;
+                        register_write <= 1'b0;
+                    end
+
+                    BNE:
+                    begin
+                        alu_opcode[0] <= 1'b1;
+                        alu_opcode[1] <= 1'b0;
+                        branch_ne <= 1'b1;
+                        register_write <= 1'b0;
+                    end
+
+                    SW: 
+                    begin
+                        memory_write <= 1'b1;
+                        alu_opcode[1] <= 1'b0;
+                        alu_source <= 1'b1;
+                        register_write <= 1'b0;
+                    end
+
+                    ADDI: 
+                    begin
+                        register_destination <= 1'b0;
+                        alu_opcode[1] <= 1'b0;
+                        alu_source <= 1'b1;
+                    end
+
+                    ADD: 
+                    begin
+                    end
+
+                    JUMP: 
+                    begin
+                        jump <= 1'b1;
+                    end
+                endcase
+            end
+        endmodule
+
+`endif
--- datapath/ext.v
+++
@@ -1,4 +0,0 @@
-module ext (input [15:0] a,
-                     output [31:0] y);
-  assign y = {{16{a[15]}}, a};
-endmodule
