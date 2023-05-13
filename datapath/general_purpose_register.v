e22b8e4537e075febb7e31545ecc56921c19dca8
commandblock2 <tony_hu@live.com>
2023-05-13 19:45:28 +0800

    fix gpr

--- datapath/general_purpose_register.v
+++ datapath/general_purpose_register.v
@@ -1,14 +1,20 @@
+/*
+ * General purpose registers with 2 output port (read) and 1 input port
+*/
 `ifndef _general_purpose_register
 `define _general_purpose_register
 module general_purpose_register (
         input system_clock,
         input write_enable,
-        input [4:0] read_address_1, read_address_2, write_address,
+
+        input write_address,
         input [31:0] write_data,
+        
+        input [4:0] read_address_1, read_address_2, 
         output [31:0] read_data_1, read_data_2
     );
 
-    reg [31:0] register_file[31:0];
+    reg [31:0] registers[31:0];
 
     // Three-ported register file
     // Read two ports combinationally
@@ -18,12 +24,12 @@ module general_purpose_register (
     begin
         if (write_enable)
         begin
-            register_file[write_address] <= write_data;
+            registers[write_address] <= write_data;
         end
     end
 
-    assign read_data_1 = (read_address_1 != 0) ? register_file[read_address_1] : 0;
-    assign read_data_2 = (read_address_2 != 0) ? register_file[read_address_2] : 0;
+    assign read_data_1 = (read_address_1 != 0) ? registers[read_address_1] : 0;
+    assign read_data_2 = (read_address_2 != 0) ? registers[read_address_2] : 0;
 
 endmodule
 
