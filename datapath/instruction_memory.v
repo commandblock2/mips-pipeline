a7bcde29d1cecb043fbeeb87e9ca95f1b87e4480
commandblock2 <tony_hu@live.com>
2023-05-16 13:57:02 +0800

    now im is instant

--- datapath/instruction_memory.v
+++ datapath/instruction_memory.v
@@ -1,15 +1,14 @@
 `ifndef _instruction_memory
 `define _instruction_memory
         module instruction_memory #(
-            parameter SIZE_EXP2 = 10
-        )(
+                parameter SIZE_EXP2 = 10
+            )(
                 input system_clock,
                 input [SIZE_EXP2 - 1:0] address,
-                output reg [31:0] data_out
+                output [31:0] data_out
             );
             reg [31:0] memory [0:2 ^ SIZE_EXP2 - 1];
-
-            always @ (posedge system_clock)
-                data_out <= memory[address];
+            
+            assign  data_out = memory[address];
         endmodule
 `endif
