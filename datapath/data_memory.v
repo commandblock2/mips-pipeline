c5f50ebecd23472b3692937f5aa94070ebe98374
commandblock2 <tony_hu@live.com>
2023-05-17 10:08:24 +0800

    1kb data mem

--- datapath/data_memory.v
+++ datapath/data_memory.v
@@ -1,15 +1,15 @@
 `ifndef _data_memory
 `define _data_memory
 
-module data_memory(
+module data_memory (
     input system_clock,
     input write_enable,
     input [31:0] address,
     input [31:0] write_data,
-    output reg [31:0] read_data
+    output [31:0] read_data
 );
 
-    reg [31:0] memory[63:0];
+    reg [31:0] memory[1023:0];
 
     // Read operation
     assign read_data = memory[address[31:2]];
